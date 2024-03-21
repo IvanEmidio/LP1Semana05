14425d3c889cf9750dd63b8032f4ff7aec2acb43
IvanEmidio <ivan.emidio@live.com.pt>
2024-03-21 16:29:26 +0000

    Exercicio Chronos

---
+++ Chronos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(600);
+            crono2.Start();
+            Thread.Sleep(200);
+            crono1.Stop();
+            crono2.Stop();
+
+            long a = crono1.ElapsedMilliseconds;
+            long b = crono2.ElapsedMilliseconds;
+
+            double seg1 = a / 1000.0;
+            double seg2 = b / 1000.0;
+
+            Console.WriteLine(seg1);
+            Console.WriteLine(seg2);
+        }
+    }
+}
