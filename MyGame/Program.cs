91ad4e1aaf1b2fd00e17426730c01b2bbd564507
Ivan <ivan.emidio@live.com.pt>
2024-03-22 08:59:17 +0000

    Exercicio 10 completo

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,27 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int n = int.Parse(args[0]);
+            Enemy [] enemies = new Enemy [n];
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Nome do inimigo {i + 1}");
+                string name = Console.ReadLine();
+                Enemy enemy = new Enemy(name);
+                enemies[i] = enemy;
+                
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{enemies[i].GetName()}" +
+                $"{enemies[i].GetHealth()} {enemies[i].GetShield()}");
+            }
+            
+
         }
     }
 }
