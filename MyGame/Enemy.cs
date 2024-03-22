266bf92c359e3ff1091e710aad0e540ed16abf0a
Ivan <ivan.emidio@live.com.pt>
2024-03-22 08:29:41 +0000

    Add construtor to Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -3,6 +3,15 @@ namespace MyGame
 
     public class Enemy
     {
-        
+        private string name;
+        private float health = 100;
+        private float shield = 0;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
