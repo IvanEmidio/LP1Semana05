58ac57e4f7ef62031c67f499a625553479d4b729
Ivan <ivan.emidio@live.com.pt>
2024-03-22 08:20:47 +0000

    Exercicios Dados

---
+++ RanDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            int dado = 0;
+
+            Random r = new Random(s);
+
+            for (int x = 0; x < n; x++)
+            {
+                dado += r.Next(1,7);
+            }
+
+            Console.WriteLine(dado);
+        }
+    }
+}
