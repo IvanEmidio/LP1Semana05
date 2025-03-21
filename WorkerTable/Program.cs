a4e4a25cf06b7646194f383a3cf4b9e5a04e526b
IvanEmidio <ivan.emidio@live.com.pt>
2025-03-21 14:51:19 +0000

    Class Exercise

---
+++ WorkerTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Spectre.Console;
+using Bogus;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            Table table = new Table();
+            Faker testUsers = new Faker("pt_PT");
+            Faker testJobs = new Faker();
+
+            table.AddColumn("ID");
+            table.AddColumn(new TableColumn("Name"));
+            table.AddColumn(new TableColumn("Job"));
+
+            foreach (int i in args[0])
+            {
+                table.AddRow(i, testUsers, testJobs);
+            }
+            
+            AnsiConsole.Write(table);
+        }
+    }
+}
