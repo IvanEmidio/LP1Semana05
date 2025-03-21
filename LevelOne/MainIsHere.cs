4150375ca4dc7094081422e67e698b3688e9392f
IvanEmidio <ivan.emidio@live.com.pt>
2025-03-21 11:37:53 +0000

    Add color print

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -11,8 +11,9 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Bad Day");
+            Console.WriteLine("Nice Day");
             Console.WriteLine("Bye!");
+
         }
 
         private static void Method()
